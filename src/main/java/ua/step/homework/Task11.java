b8b277e470779f431c01a58da8cfb0b18bedea93
sergeybiophys <rove2hati@gmail.com>
2021-12-15 18:24:03 +0200

    task11 completed

--- src/main/java/ua/step/homework/Task11.java
+++ src/main/java/ua/step/homework/Task11.java
@@ -9,6 +9,29 @@ package ua.step.homework;
 public class Task11 {
     public static void main(String[] args) {
         // TODO: Здесь Ваш код. Выводите числа и слова через пробел
+        funnyFunction();
+    }
 
+    public static void funnyFunction()
+    {
+        for(int i = 1; i<1000;i++)
+        {
+            if(i%3==0&&i%5!=0)
+            {
+                System.out.print("fizz"+" ");
+            }
+            else if(i%5==0&&i%3!=0)
+            {
+                System.out.print("buzz"+" ");
+            }
+            else if(i%15==0)
+            {
+                System.out.print("hiss"+" ");
+            }
+            else
+            {
+                System.out.print(i+" ");
+            }
+        }
     }
 }
