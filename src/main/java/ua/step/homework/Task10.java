662a73bc73494c704c52cd8b2fdf842a109a1398
sergeybiophys <rove2hati@gmail.com>
2021-12-15 19:29:05 +0200

    task10 completed

--- src/main/java/ua/step/homework/Task10.java
+++ src/main/java/ua/step/homework/Task10.java
@@ -11,5 +11,29 @@ public class Task10 {
 
     public static void main(String[] args) {
         // TODO: Здесь Ваш код.
+        long result = leastCommonMultiple();
+        System.out.print(result);
+    }
+
+    public static long gcd(long a, long b)//greatest common divisor
+    {
+        return (b==0)?a:gcd(b,a%b);
+    }
+
+    public static long lcm(long a, long b)//Least common multiple
+    {
+        return Math.abs(a*b)/gcd(a,b);
+    }
+    public static long leastCommonMultiple()
+    {
+        long commonMultiple = 1;
+        int [] items = new int[] {1,2,3,4,5,6,7,8,9,10,
+                                  11,12,13,14,15,16,17,
+                                              18,19,20};
+        for(int item: items)
+        {
+            commonMultiple = lcm(commonMultiple,item);
+        }
+        return commonMultiple;
     }
 }
